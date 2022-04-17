c6d122f80824bf08080498abad7019e7d1ece656
afifrivay29 <afifrivaykusnanto29@gmail.com>
2022-04-17 23:55:45 +0700

    add loading react-spinners

---
+++ my-movie/src/App.js
@@ -0,0 +1,48 @@
+import React, {useState, useEffect} from 'react';
+import logo from './logo.svg';
+import './App.css';
+import ClipLoader from "react-spinners/RingLoader";
+
+function App() {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true)
+    setTimeout(() => {
+      setLoading(false)
+    }, 8000)
+  }, [])
+  return (
+    <div className="App">
+      {
+        loading ? (
+
+        <ClipLoader 
+        color={"#4A90E2"} 
+        loading={loading} 
+        size={60} 
+        />
+        )
+        :
+        (
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+        )
+      }
+    </div>
+  );
+}
+
+export default App;
